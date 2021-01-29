f39491b18d5f46a4a0ccf8550e57283d034f50f6
Promise Fru <promisefru1@gmail.com>
2021-01-29 13:37:56 +0100

    rectangle structure built

--- shape_calculator.py
+++ shape_calculator.py
@@ -1,4 +1,26 @@
 class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    
+    def set_width(self):
+
+    def set_height(self):
+
+    def get_area(self):
+        return (self.width*self.height)
+    
+    def get_perimater(self):
+        return(2 * width + 2 * height)
+
+    def get_diagonal(self):
+        return((width ** 2 + height ** 2) ** .5)
+
+    def get_picture(self):
+
+    def get_amount_inside(self):
+
+
 
 
 
